a45c0c314ee022118dd5dc847a9d43a0bd2baf6b
MhNirra <72897503+MhNirra@users.noreply.github.com>
2022-10-11 21:35:27 -0300

    Add files via upload

---
+++ a/js/mol.js
@@ -0,0 +1,53 @@
+$(function() {
+  var $window = $(window),
+      $doc = $(document),
+      $html = $('html'),
+      $body = $('body'),
+      $container = $('#container'),
+      $music = new Audio();
+
+  if ($music.canPlayType('audio/mpeg;')) {
+      $music.type= 'audio/mpeg';
+      $music.src= '/a/music.mp3';
+	} else {
+      $music.type= 'audio/ogg';
+      $music.src= '/a/music.ogg';
+	}
+	$music.volume = 0;
+
+	$doc.on('keydown', function(event) {
+      if (event.which == 32) {
+          $window.trigger('advance');
+      };
+	});
+
+    $doc.on('touchstart', function(event) {
+        $window.trigger('advance');
+    });
+
+    $window.on('advance', function() {
+        var $frame = $container.find('.frame'),
+            $current = $container.find('.current'),
+            $next = $frame.eq($current.index() + 1);
+
+        if ($current.hasClass('final'))
+            return;
+
+        if ($next.hasClass('start-music')) {
+            $music.play();
+            $($music).animate({volume: 1}, 2000);
+        }
+
+        if (Modernizr.csstransitions) {
+            $current.removeClass('current').addClass('spent');
+            $next.addClass('current');
+        } else {
+            if ($current.hasClass('slow-hack')) {
+                $current.css({ opacity: 1 }).removeClass('current').animate({ opacity: 0 }, 1500);
+            } else {
+                $current.css({ opacity: 1 }).removeClass('current').animate({ opacity: 0 }, 500);
+            }
+            $next.css({ opacity: 0 }).addClass('current').animate({ opacity: 1 }, 1000);
+        }
+    });
+});
